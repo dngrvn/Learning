459d0ea974f7c644319d19f9d33e29ec46f0c281
dngrvn <108631571+dngrvn@users.noreply.github.com>
2024-08-24 19:13:25 +0400

    Add files via upload

---
+++ C#/homework_2/Zadacha_13/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num <= 99)
+   {Console.WriteLine("Третьей цифры нет");}
+else
+   {Console.WriteLine(num % 10); 
+}
---
+++ C#/homework_2/Zadacha_15/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Enter number day of week: ");
+int i = int.Parse(Console.ReadLine());
+
+if (i < 6)
+Console.WriteLine("Рабочий день");
+if (i == 6)
+Console.WriteLine("Выходной");
+if (i == 7)
+Console.WriteLine("Выходной");
+if (i > 7)
+Console.WriteLine("Нет такого номера дня в неделе");
